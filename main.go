12f5b7d09d51d177c5c3e657e215e75856ad4b84
TimLu <lutim102030@gmail.com>
2020-02-19 23:39:26 +0800

    add request

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,8 +10,24 @@ import (
 func main() {
 
 	http.HandleFunc("/deploy-uat", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		triggerJob(w, "")
 	})
 
-	log.Fatal(http.ListenAndServe(":8881", nil))
+	http.HandleFunc("/deploy-prod", func(w http.ResponseWriter, r *http.Request) {
+		triggerJob(w, "")
+	})
+
+	log.Fatal(http.ListenAndServe(":8882", nil))
+}
+
+func triggerJob(w http.ResponseWriter, url string) {
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Fprintf(w, string(body))
 }
